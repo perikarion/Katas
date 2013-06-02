fb149d33838361e48c2da95ed1bfac4c5818be04
perikarion <perikarion@gmail.com>
2013-06-02 15:22:58 +0200

    compositional kata

--- Composition/CS/Composition/Algorithm.Tests/Composition/AggregationTests.cs
+++ Composition/CS/Composition/Algorithm.Tests/Composition/AggregationTests.cs
@@ -1,4 +1,5 @@
 ﻿using Algorithm.Composition;
+using Algorithm.Inheritance;
 using Xunit;
 
 namespace Algorithm.Tests.Composition
@@ -8,7 +9,7 @@ namespace Algorithm.Tests.Composition
         [Fact]
         public void SummingAggregation_Produces_Sum()
         {
-            var aggregator = new PointsAggregator(_measurements, new EmptyFilter(), new SummingStrategy());
+            var aggregator = new Algorithm.Composition.PointsAggregator(_measurements, new EmptyFilter(), new SummingStrategy());
 
             var result = aggregator.Aggregate();
 
@@ -19,7 +20,7 @@ namespace Algorithm.Tests.Composition
         [Fact]
         public void AveragingAggregagtor_Produces_Average()
         {
-            var aggregator = new PointsAggregator(_measurements, new EmptyFilter(), new AveragingStrategy());
+            var aggregator = new Algorithm.Composition.PointsAggregator(_measurements, new EmptyFilter(), new AveragingStrategy());
 
             var result = aggregator.Aggregate();
 
@@ -30,7 +31,7 @@ namespace Algorithm.Tests.Composition
         [Fact]
         public void LowPassAveragingAggregator_Applys_Filter()
         {
-            var aggregator = new PointsAggregator(_measurements, new LowPassFilter(), new AveragingStrategy());
+            var aggregator = new Algorithm.Composition.PointsAggregator(_measurements, new LowPassFilter(), new AveragingStrategy());
 
             var result = aggregator.Aggregate();
 
@@ -40,29 +41,29 @@ namespace Algorithm.Tests.Composition
 
         // Uncomment this test and make it pass reusing as much code as 
         // possible from other classes that are available in the Composition folder
-        //[Fact]
-        //public void HighPassSummingAggregator_Applys_Filter()
-        //{                
-        //    var aggregator = new PointsAggregator(_measurements, new HighPassFilter(), new SummingStrategy());
+        [Fact]
+        public void HighPassSummingAggregator_Applys_Filter()
+        {
+            var aggregator = new Algorithm.Composition.PointsAggregator(_measurements, new HighPassFilter(), new SummingStrategy());
 
-        //    var result = aggregator.Aggregate();
+            var result = aggregator.Aggregate();
 
-        //    Assert.Equal(105, result.X);
-        //    Assert.Equal(15, result.Y);
-        //}
+            Assert.Equal(105, result.X);
+            Assert.Equal(15, result.Y);
+        }
 
         /// Uncomment this test and make it pass by building a new class to 
         /// hide the "composition" of strategies and filters ...
-        //[Fact]
-        //public void CustomHighPassSummingAggregator_Applys_Filter()
-        //{                
-        //    var aggregator = new HighPassSummingAggregator(_measurements);
+        [Fact]
+        public void CustomHighPassSummingAggregator_Applys_Filter()
+        {
+            var aggregator = new HighPassSummingAggregator(_measurements);
 
-        //    var result = aggregator.Aggregate();
+            var result = aggregator.Aggregate();
 
-        //    Assert.Equal(105, result.X);
-        //    Assert.Equal(15, result.Y);
-        //}
+            Assert.Equal(105, result.X);
+            Assert.Equal(15, result.Y);
+        }
 
         Measurement[] _measurements = new[]
         {
